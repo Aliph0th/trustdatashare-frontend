a3edc1d01cf9490729ee86c02f7adfa00975a8ea
Aliph0th <theblacklord05@gmail.com>
2025-05-08 18:59:21 +0300

    fix: user is loading initially

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -3,7 +3,7 @@ import { User } from '../types';
 import { UserContext } from './contexts';
 export const UserContextProvider: FC<PropsWithChildren> = ({ children }) => {
    const [user, setUser] = useState<User | null>();
-   const [isUserLoading, setIsUserLoading] = useState<boolean>(false);
+   const [isUserLoading, setIsUserLoading] = useState<boolean>(true);
 
    return (
       <UserContext.Provider value={{ user, isUserLoading, setUser, setIsUserLoading }}>{children}</UserContext.Provider>
